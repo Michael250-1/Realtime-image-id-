742364f9213096e87f32fe6e1276bbdb6fef1826
Michael250-1 <79066678+Michael250-1@users.noreply.github.com>
2021-10-25 17:59:43 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier=ml5.imageClassifier("MobileNet",modelLoaded);
 }
+function modelLoaded(){
+  console.log("model has loaded");
+}
+function draw(){
+  image(video,0,0,300,300)
+  classifier.classify(video,gotResult);
+}
+var previousresult="";
 
-
+function gotResult(error,results){
+  console.log("abc")
+  if(error){
+    console.error(error);
+  }
+  else {
+if((results[0].confidence>0.5)&&(previousresult!=results[0].label)) {
+  console.log(results);
+  previousresult=results[0].label;
+  var synth=window.SpeechSynthesis;
+  speak_data="object detected is " + results[0].label;
+  var utterthis=new SpeechSynthesisUtterance(speak_data);
+  synth.speak(utterthis);
+  document.getElementById("resultname").innerHTML=results[0].label;
+  document.getElementById("resultaccuracy").innerHTML=results[0].confidence;
+}
+    
+  }
+}
 
